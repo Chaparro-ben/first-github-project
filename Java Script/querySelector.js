e8fe393d5dc52e4e705e78ecdde550b1be9e748a
Ben Batres <110201584+Chaparro-ben@users.noreply.github.com>
2024-09-23 11:13:05 -0700

    Delete Java Script directory

--- Java Script/querySelector.js
+++
@@ -1,7 +0,0 @@
-function public() {
-    const el = document.querySelector("#prompt"); // To select a specific element, we add a selector inside parentheses. For example the p tag: *.QuerySelector("p")* 
-    //0querySelector only retreeves the first element
-
-    el.innerHTML = "Website published!";
-    document.get
-}
